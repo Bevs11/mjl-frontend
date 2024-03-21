fc62cd9668159d94c814a5a3c3513ddd016bfaf1
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2024-03-21 23:12:04 +0800

    fix: created crop image

--- src/app/components/ImageCropper.tsx
+++ src/app/components/ImageCropper.tsx
@@ -1,57 +1,47 @@
-import { useRef, useState } from "react";
+import React, { useState } from "react";
 import ReactCrop, {
-    centerCrop,
-    convertToPixelCrop,
     makeAspectCrop,
+    type Crop,
+    centerCrop,
 } from "react-image-crop";
-import setCanvasPreview from "../../lib/setCanvasPreview";
 
+const MIN_Dimensions = 150;
 const ASPECT_RATIO = 1;
-const MIN_DIMENSION = 150;
 
-const ImageCropper = ({
-    closeModal,
-    updateAvatar,
-}: {
-    closeModal: any;
-    updateAvatar: any;
-}) => {
-    const imgRef = useRef(null);
-    const previewCanvasRef = useRef(null);
-    const [imgSrc, setImgSrc] = useState("");
-    const [crop, setCrop] = useState();
-    const [error, setError] = useState("");
+const ImageCropper = () => {
+    const [imgSrc, setImgSrc] = useState<string | ArrayBuffer | null>("");
+    const [crop, setCrop] = useState<Crop>();
+    const [error, setError] = useState<string>("");
 
-    const onSelectFile = (e: { target: { files: any[] } }) => {
-        const file = e.target.files?.[0];
+    const onSelectFile = (e: any) => {
+        const file = e.target.files[0];
         if (!file) return;
-
         const reader = new FileReader();
-        reader.addEventListener("load", () => {
+        reader.addEventListener("load", (event) => {
             const imageElement = new Image();
-            const imageUrl = reader.result?.toString() || "";
-            imageElement.src = imageUrl;
+            const imageURL = reader.result?.toString() || "";
+            imageElement.src = imageURL;
 
             imageElement.addEventListener("load", (e) => {
                 if (error) setError("");
                 const { naturalWidth, naturalHeight } =
                     e.currentTarget as HTMLImageElement;
                 if (
-                    naturalWidth < MIN_DIMENSION ||
-                    naturalHeight < MIN_DIMENSION
+                    naturalWidth < MIN_Dimensions ||
+                    naturalHeight < MIN_Dimensions
                 ) {
-                    setError("Image must be at least 150 x 150 pixels.");
-                    return setImgSrc("");
+                    setError("image must be atleast 150 x 150 px");
+                    setImgSrc("");
+                    return;
                 }
             });
-            setImgSrc(imageUrl);
+            setImgSrc(imageURL);
         });
         reader.readAsDataURL(file);
     };
-
-    const onImageLoad = (e: { currentTarget: { width: any; height: any } }) => {
+    const onImageLoad = (e: any) => {
         const { width, height } = e.currentTarget;
-        const cropWidthInPercent = (MIN_DIMENSION / width) * 100;
+        const cropWidthInPercent = (MIN_Dimensions / width) * 100;
 
         const crop = makeAspectCrop(
             {
@@ -65,11 +55,13 @@ const ImageCropper = ({
         const centeredCrop = centerCrop(crop, width, height);
         setCrop(centeredCrop);
     };
-
     return (
-        <>
+        <div className="py-10">
             <label className="block mb-3 w-fit">
-                <span className="sr-only">Choose profile photo</span>
+                <span className="text-white font-bold">
+                    Choose Profile Photo
+                </span>
+
                 <input
                     type="file"
                     accept="image/*"
@@ -77,7 +69,7 @@ const ImageCropper = ({
                     className="block w-full text-sm text-slate-500 file:mr-4 file:py-1 file:px-2 file:rounded-full file:border-0 file:text-xs file:bg-gray-700 file:text-sky-300 hover:file:bg-gray-600"
                 />
             </label>
-            {error && <p className="text-red-400 text-xs">{error}</p>}
+            {error && <div className="text-red-500">{error}</div>}
             {imgSrc && (
                 <div className="flex flex-col items-center">
                     <ReactCrop
@@ -88,52 +80,22 @@ const ImageCropper = ({
                         circularCrop
                         keepSelection
                         aspect={ASPECT_RATIO}
-                        minWidth={MIN_DIMENSION}
+                        minWidth={MIN_Dimensions}
                     >
                         <img
-                            ref={imgRef}
-                            src={imgSrc}
-                            alt="Upload"
-                            style={{ maxHeight: "70vh" }}
+                            src={imgSrc.toString()}
+                            alt="avatar"
+                            style={{ maxHeight: "800px" }}
                             onLoad={onImageLoad}
                         />
                     </ReactCrop>
-                    <button
-                        className="text-white font-mono text-xs py-2 px-4 rounded-2xl mt-4 bg-sky-500 hover:bg-sky-600"
-                        onClick={() => {
-                            setCanvasPreview(
-                                imgRef.current, // HTMLImageElement
-                                previewCanvasRef.current, // HTMLCanvasElement
-                                convertToPixelCrop(
-                                    crop,
-                                    imgRef.current.width,
-                                    imgRef.current.height
-                                )
-                            );
-                            const dataUrl =
-                                previewCanvasRef.current.toDataURL();
-                            updateAvatar(dataUrl);
-                            closeModal();
-                        }}
-                    >
+                    <button className="text-white font-mono text-xs py-2 px-4 rounded-2xl mt-4 bg-sky-500 hover:bg-sky-600">
                         Crop Image
                     </button>
                 </div>
             )}
-            {crop && (
-                <canvas
-                    ref={previewCanvasRef}
-                    className="mt-4"
-                    style={{
-                        display: "none",
-                        border: "1px solid black",
-                        objectFit: "contain",
-                        width: 150,
-                        height: 150,
-                    }}
-                />
-            )}
-        </>
+        </div>
     );
 };
+
 export default ImageCropper;
