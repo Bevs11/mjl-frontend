7b621ab3d2b16b6292945fbc2a3def685babe747
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2024-05-27 20:28:19 +0800

    created navbar styling

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,39 @@
 import React from "react";
-import LogoSmall from "../../../../public/homepage/hero-image.svg";
+import Image from "next/image";
+
+import LogoSmall from "../../../public/mjl-logo.svg";
+import Magnfyingglass from "../../../public/navbar/magnifying-glass.svg";
 
 export default function Navbar() {
     return (
-        <div className="h-[56px] bg-customDarkBlue ">
-            <div className="px-2 sm:px-8 md:px-12 lg:px-[64px]">
-                <div></div>
+        <div className="h-[56px] bg-customDarkBlue">
+            <div className="h-full px-2 sm:px-8 md:px-12 lg:px-[64px] flex items-center justify-between">
+                <div className="flex gap-1">
+                    <Image
+                        src={LogoSmall}
+                        alt="mjl logo small"
+                        width={39}
+                        height={39}
+                    />
+                    <div className="font-bold text-2xl text-white my-auto">
+                        MJL ONLINE
+                    </div>
+                </div>
+                <div className="flex gap-6 text-white">
+                    <div>CATEGORIES</div>
+                    <div>WEBINARS</div>
+                    <div>CERTIFICATE</div>
+                </div>
+                <div className="bg-white/10 py-2 px-2 rounded w-full max-w-[500px] flex">
+                    <Image
+                        src={Magnfyingglass}
+                        alt="magnifying glass"
+                        width={20}
+                        height={20}
+                    />
+                    <input type="text" className="w-full bg-transparent" />
+                </div>
+                <button className="btn-sky-blue">Create Account</button>
             </div>
         </div>
     );
