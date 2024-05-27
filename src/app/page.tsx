8bc74452be84393ec5097b6de2efaddd115f1c5d
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2024-05-27 16:05:14 +0800

    commented out image-cropper to be used later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,106 +5,84 @@ import ImageCropper from "./components/ImageCropper";
 import "react-image-crop/dist/ReactCrop.css";
 
 export default function Home() {
-    return (
-        <main className="relative  flex flex-col items-center">
-            <div className="font-bold text-[38px]">HELLO MJL</div>
-            <InputImage />
-        </main>
-    );
+    return <main></main>;
 }
 
-const InputImage = () => {
-    const avatarURL = useRef(
-        "https://ami.animecharactersdatabase.com/uploads/chars/67712-371300562.png"
-    );
-    const [modalOpen, setModalOpen] = useState(false);
+// const InputImage = () => {
+//     const avatarURL = useRef(
+//         "https://ami.animecharactersdatabase.com/uploads/chars/67712-371300562.png"
+//     );
+//     const [modalOpen, setModalOpen] = useState(false);
 
-    const handleCloseModal = () => {
-        setModalOpen(false);
-    };
+//     const handleCloseModal = () => {
+//         setModalOpen(false);
+//     };
 
-    const updateAvatar = (imageSrc: string) => {
-        avatarURL.current = imageSrc;
-        setModalOpen(false);
-    };
-    return (
-        <div className="flex flex-col mt-20 gap-10 items-center">
-            <div className="font-bold">INPUT IMAGE HERE</div>
-            {/* Display Image */}
-            <div>
-                <img
-                    src={avatarURL.current}
-                    alt="avatar"
-                    className="w-[300px] h-[300px]"
-                />
-            </div>
-            <button
-                title="change photo"
-                className="bg-blue-500 text-white px-4 py-2 rounded-md"
-                onClick={(e) => setModalOpen((prev) => !prev)}
-            >
-                Edit Image
-            </button>
-            {modalOpen && (
-                <ImageEditingModal
-                    handleCloseModal={handleCloseModal}
-                    updateAvatar={updateAvatar}
-                />
-            )}
-        </div>
-    );
-};
-
-const ImageEditingModal = ({
-    handleCloseModal,
-    updateAvatar,
-}: {
-    handleCloseModal: any;
-    updateAvatar: any;
-}) => {
-    return (
-        <div
-            className="relative z-10"
-            aria-labelledby="crop-image-dialog"
-            role="dialog"
-            aria-modal="true"
-        >
-            <div className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-all backdrop-blur-sm"></div>
-            <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                <div className="flex min-h-full justify-center px-2 py-12 text-center ">
-                    <div className="relative w-[95%] sm:w-[80%] min-h-[60vh] rounded-2xl bg-gray-800 text-slate-100 text-left shadow-xl transition-all">
-                        <div className="px-5 py-4 bg-none">
-                            <button
-                                type="button"
-                                className="border border-2 border-solid border-black px-4 py-2 rounded-md bg-red-500 text-white"
-                                onClick={handleCloseModal}
-                            >
-                                Close Modal
-                            </button>
-                            <ImageCropper
-                                updateAvatar={updateAvatar}
-                                handleCloseModal={handleCloseModal}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
-
-// const ImageEditingModal = ({ handleCloseModal }: { handleCloseModal: any }) => {
+//     const updateAvatar = (imageSrc: string) => {
+//         avatarURL.current = imageSrc;
+//         setModalOpen(false);
+//     };
 //     return (
-//         <div className="absolute top-0 left-0 w-screen h-screen flex flex-col items-center justify-center ">
-//             <div className="w-1/2 h-1/2 bg-blue-500 p-6">
-//                 <button
-//                     onClick={() => handleCloseModal()}
-//                     className="border border-2 border-solid border-black px-4 py-2 rounded-md bg-red-500 text-white"
-//                 >
-//                     Close Modal
-//                 </button>
-//                 <ImageCropper />
-//                 <div className="my-4"></div>
+//         <div className="flex flex-col mt-20 gap-10 items-center">
+//             <div className="font-bold">INPUT IMAGE HERE</div>
+//             {/* Display Image */}
+//             <div>
+//                 <img
+//                     src={avatarURL.current}
+//                     alt="avatar"
+//                     className="w-[300px] h-[300px]"
+//                 />
+//             </div>
+//             <button
+//                 title="change photo"
+//                 className="bg-blue-500 text-white px-4 py-2 rounded-md"
+//                 onClick={(e) => setModalOpen((prev) => !prev)}
+//             >
+//                 Edit Image
+//             </button>
+//             {modalOpen && (
+//                 <ImageEditingModal
+//                     handleCloseModal={handleCloseModal}
+//                     updateAvatar={updateAvatar}
+//                 />
+//             )}
+//         </div>
+//     );
+// };
+
+// const ImageEditingModal = ({
+//     handleCloseModal,
+//     updateAvatar,
+// }: {
+//     handleCloseModal: any;
+//     updateAvatar: any;
+// }) => {
+//     return (
+//         <div
+//             className="relative z-10"
+//             aria-labelledby="crop-image-dialog"
+//             role="dialog"
+//             aria-modal="true"
+//         >
+//             <div className="fixed inset-0 bg-gray-900 bg-opacity-75 transition-all backdrop-blur-sm"></div>
+//             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+//                 <div className="flex min-h-full justify-center px-2 py-12 text-center ">
+//                     <div className="relative w-[95%] sm:w-[80%] min-h-[60vh] rounded-2xl bg-gray-800 text-slate-100 text-left shadow-xl transition-all">
+//                         <div className="px-5 py-4 bg-none">
+//                             <button
+//                                 type="button"
+//                                 className="border border-2 border-solid border-black px-4 py-2 rounded-md bg-red-500 text-white"
+//                                 onClick={handleCloseModal}
+//                             >
+//                                 Close Modal
+//                             </button>
+//                             <ImageCropper
+//                                 updateAvatar={updateAvatar}
+//                                 handleCloseModal={handleCloseModal}
+//                             />
+//                         </div>
+//                     </div>
+//                 </div>
 //             </div>
 //         </div>
 //     );
