d51da980f8a68187cae676f1f986ffe28c179041
Bevs11 <123889911+Bevs11@users.noreply.github.com>
2024-05-27 19:13:55 +0800

    created navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LogoSmall from "../../../../public/homepage/hero-image.svg";
+
+export default function Navbar() {
+    return (
+        <div className="h-[56px] bg-customDarkBlue ">
+            <div className="px-2 sm:px-8 md:px-12 lg:px-[64px]">
+                <div></div>
+            </div>
+        </div>
+    );
+}
